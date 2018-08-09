fc303e8fc75094cccde958ab77d07419e164e2ff
aboud20-meet <aboud20@meet.mit.edu>
2018-08-09 13:44:23 +0300

    finished part 3

--- exercises/app.py
+++ exercises/app.py
@@ -1,5 +1,5 @@
 from databases import *
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,14 +9,17 @@ def home():
 @app.route('/student/<int:student_id>')
 def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
-@app.route('/add', method=['GET','POST']) 
+@app.route('/add', methods=['GET','POST']) 
 def add_student_route():
 	if request.method=='GET':
 		return render_template('add.html')
 	elif request.method=='POST':
 		print('Received POST request!')
+		student_name = request.form['student_name']
+		year= request.form['student_year']
+		add_student(student_name, int(year),False)
+		print(student_name)
 		return render_template('add.html')
-
 
 	
 
