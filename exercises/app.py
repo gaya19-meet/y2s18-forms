b95ab72c72a420ed13c4f693f7886a9ba9ca6ed2
aboud20-meet <aboud20@meet.mit.edu>
2018-08-09 12:21:51 +0300

    started part 1  2  and has an error n part 2

--- exercises/app.py
+++ exercises/app.py
@@ -9,5 +9,16 @@ def home():
 @app.route('/student/<int:student_id>')
 def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
+@app.route('/add', method=['GET','POST']) 
+def add_student_route():
+	if request.method=='GET':
+		return render_template('add.html')
+	elif request.method=='POST':
+		print('Received POST request!')
+		return render_template('add.html')
+
+
+	
+
 
 app.run(debug=True)
